966a7d825621335d1b2db976c7e8622315bc9855
TheBiggestMatt <43455189+TheBiggestMatt@users.noreply.github.com>
2018-09-20 18:34:00 -0400

    Add files via upload
    first upload

---
+++ P2T1_SalesPrediction_MatthewSawyerpy.py
@@ -0,0 +1,13 @@
+# sales prediction progam 
+# 9/20/2018
+# CTI-110 P2T1-sales prediction
+# Matthew Sawyer
+#
+
+
+
+total_sales = float(input('Enter the projected sales: '))
+
+profit = total_sales * 0.23
+
+print('The profit is $', format(profit, ',.2f'))
